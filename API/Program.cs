db42d9a21287c74d6e30870daf595b4c489cf805
Pathik Modi <modipathik95@gmail.com>
2024-04-18 09:04:36 +1200

    Add cors certificate middleware to fetch data from API url

--- API/Program.cs
+++ API/Program.cs
@@ -11,14 +11,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
